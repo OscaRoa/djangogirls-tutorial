60bc01f6c55961b7a5e0ab81ca1ed3445106c938
Oscar Roa <oshardy3@gmail.com>
2016-01-24 23:15:41 -0600

    Add views, templates and urls to edit post inside the site

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/new/$', views.new_post, name='new_post'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+
 ]
